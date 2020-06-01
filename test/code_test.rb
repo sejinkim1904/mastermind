f833103325d3488f79d4edeceae18231ac1d2ca9
Sejin Kim <sejin.kim1904@gmail.com>
2020-06-01 16:03:22 -0600

    Add test to see if code has 4 pegs

--- test/code_test.rb
+++ test/code_test.rb
@@ -1,5 +1,6 @@
 require './test/test_helper'
 require './lib/code'
+require './lib/peg'
 
 class CodeTest < MiniTest::Test
   def setup
@@ -8,5 +9,9 @@ class CodeTest < MiniTest::Test
 
   def test_it_exists
     assert_instance_of Code, @code
+  end
+
+  def test_it_has_pegs
+    assert_equal @code.pegs.count, 4
   end
 end
